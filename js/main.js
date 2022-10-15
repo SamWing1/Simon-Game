3008764ccdfde284908ae51a63da182d3875073b
SamWing1 <geckoromanwing@hotmail.com>
2022-10-15 15:31:40 -0700

    buttons now depress upon being clicked

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
 //defined elements
-const green = document.querySelector('.green-space')
-const red = document.querySelector('.red-space')
-const blue = document.querySelector('.blue-space')
-const yellow = document.querySelector('.yellow-space')
+const green = document.querySelector('#grn')
+const red = document.querySelector('#red')
+const blue = document.querySelector('#blu')
+const yellow = document.querySelector('#ylw')
 let level = 1
 
 //functions
 const onSpaceClicked = function (event) {
     console.log('clicked!')
 }
+const onGreenClicked = function (event) {
+    green.classList.add('green-space-pressed')
+}
+const onRedClicked = function (event) {
+    red.classList.add('red-space-pressed')
+}
+const onBlueClicked = function (event) {
+    blue.classList.add('blue-space-pressed')
+}
+const onYellowClicked = function (event) {
+    yellow.classList.add('yellow-space-pressed')
+}
 
 //add event listener
 document.addEventListener('DOMContentLoaded', function() {
     green.addEventListener('click', onSpaceClicked)
+    green.addEventListener('click', onGreenClicked)
+
     red.addEventListener('click', onSpaceClicked)
+    red.addEventListener('click', onRedClicked)
+
     blue.addEventListener('click', onSpaceClicked)
+    blue.addEventListener('click', onBlueClicked)
+    
     yellow.addEventListener('click', onSpaceClicked)
+    yellow.addEventListener('click', onYellowClicked)
 })
 
 //start button
